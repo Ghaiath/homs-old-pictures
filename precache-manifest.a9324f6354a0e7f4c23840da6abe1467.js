39e94757fa7ad934fb6fd2def6a12a691b8ba96b
Ghaiath Altrabulsi <ghaiath@gmail.com>
2019-10-13 21:04:45 +0200

    Updates

---
+++ precache-manifest.a9324f6354a0e7f4c23840da6abe1467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597f16dc43e974b3dadbba7ea5ecaf5b",
+    "url": "/homs-old-pictures/index.html"
+  },
+  {
+    "revision": "8069f42ceac7d5bcccfb",
+    "url": "/homs-old-pictures/static/css/main.eca8f84d.chunk.css"
+  },
+  {
+    "revision": "9c7c469753de128ecee1",
+    "url": "/homs-old-pictures/static/js/2.2ed3091e.chunk.js"
+  },
+  {
+    "revision": "8069f42ceac7d5bcccfb",
+    "url": "/homs-old-pictures/static/js/main.19401868.chunk.js"
+  },
+  {
+    "revision": "e9987782e1de43e9eb9d",
+    "url": "/homs-old-pictures/static/js/runtime-main.97492f9e.js"
+  }
+]);
